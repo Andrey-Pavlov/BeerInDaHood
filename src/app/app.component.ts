83c4f67816fb31b9e79591461454c54612f14dbe
Andrey Pavlov <paffa4@gmail.com>
2017-01-24 20:30:58 +0400

    Initial files

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+/*
+ * Angular 2 decorators and services
+ */
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.css'
+  ],
+  template: `
+    <nav>
+      <a [routerLink]=" ['./'] " routerLinkActive="active">
+        Index
+      </a>
+      <a [routerLink]=" ['./home'] " routerLinkActive="active">
+        Home
+      </a>
+      <a [routerLink]=" ['./detail'] " routerLinkActive="active">
+        Detail
+      </a>
+      <a [routerLink]=" ['./barrel'] " routerLinkActive="active">
+        Barrel
+      </a>
+      <a [routerLink]=" ['./about'] " routerLinkActive="active">
+        About
+      </a>
+    </nav>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+
+    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+
+    <footer>
+      <span>Beer In Da Hood by <a [href]="url">URL name</a></span>
+      <div>
+        <a [href]="url">
+          <img [src]="" width="25%">
+        </a>
+      </div>
+    </footer>
+  `
+})
+export class AppComponent implements OnInit {
+  public src = '';
+  public name = '';
+  public url = '';
+
+  constructor(
+    public appState: AppState
+  ) {}
+
+  public ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
